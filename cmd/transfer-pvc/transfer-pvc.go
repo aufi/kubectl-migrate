741bce55bb2d4e495e2029e6a738a3b3bbd64938
Marek Aufart <maufart@redhat.com>
2026-01-28 15:04:41 +0100

    Fix PVC transfer timeout hadling
    Signed-off-by: Marek Aufart <maufart@redhat.com>

--- cmd/transfer-pvc/transfer-pvc.go
+++ cmd/transfer-pvc/transfer-pvc.go
@@ -435,7 +435,7 @@ func (t *TransferPVCCommand) run() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
-	_ = wait.PollUntilContextCancel(ctx, time.Second*5, true, func(ctx context.Context) (done bool, err error) {
+	err = wait.PollUntilContextCancel(ctx, time.Second*5, true, func(ctx context.Context) (done bool, err error) {
 		ready, err := rsyncServer.IsHealthy(ctx, destClient)
 		if err != nil {
 			log.Println(err, "unable to check rsync server health, retrying...")
@@ -443,6 +443,9 @@ func (t *TransferPVCCommand) run() error {
 		}
 		return ready, nil
 	})
+	if err != nil {
+		log.Fatal(err, "rsync server failed to become healthy")
+	}
 
 	nodeName, err := getNodeNameForPVC(srcClient, srcPVC.Namespace, srcPVC.Name)
 	if err != nil {
